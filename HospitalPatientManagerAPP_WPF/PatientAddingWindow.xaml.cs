5d1409a6ac19b34e460161355d66e42ffd5e7765
KailangHuo <hkailang@student.unimelb.edu.au>
2023-05-06 08:55:44 +0800

    Init

---
+++ HospitalPatientManagerAPP_WPF/PatientAddingWindow.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace HospitalPatientManagerAPP_WPF; 
+
+public partial class PatientAddingWindow : Window {
+    public PatientAddingWindow() {
+        InitializeComponent();
+    }
+}
